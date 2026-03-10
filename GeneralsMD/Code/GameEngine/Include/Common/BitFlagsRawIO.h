a6be2c36534fc8802dcbaf55c696b65aa2410ff4
Blue Twentyfour <blue24dev@gmail.com>
2026-03-10 02:48:10 -0400

    BitFlagsRaw::xfer simplified, minor cleanup

--- GeneralsMD/Code/GameEngine/Include/Common/BitFlagsRawIO.h
+++ GeneralsMD/Code/GameEngine/Include/Common/BitFlagsRawIO.h
@@ -1,10 +1,8 @@
-
 //MODDD - new file
 
 #pragma once
 
 #include "Common/BitFlagsRaw.h"
-//#include "Common/INI.h"
 #include "Common/Xfer.h"
 
 template <size_t NUMBITS>
@@ -12,24 +10,9 @@ void BitFlagsRaw<NUMBITS>::xfer(Xfer* xfer)
 {
 	// This is primitive enough I don't think it warrants a version, and entirely new to the as-is state of
 	// this codebase anyway.
-	if( xfer->getXferMode() == XFER_SAVE )
-	{
-		xfer->xferUser( this->m_bits, sizeof( this->m_bits ) );
-	}
-	else if( xfer->getXferMode() == XFER_LOAD )
-	{
-		xfer->xferUser( this->m_bits, sizeof( this->m_bits ) );
-	}
-	else if( xfer->getXferMode() == XFER_CRC )
-	{
-		xfer->xferUser( this->m_bits, sizeof( this->m_bits ) );
-	}
-	else
-	{
 
-		DEBUG_CRASH(( "BitFlagsXfer - Unknown xfer mode '%d'", xfer->getXferMode() ));
-		throw XFER_MODE_UNKNOWN;
-
-	}
+	// Also, skipping the 'xfer->getXferMode()' check for simplicity's sake, this same thing is fine for all
+	// expected modes of XFER_SAVE/LOAD/CRC anyway
+	xfer->xferUser( this->m_bits, sizeof( UnsignedByte ) * NUMBITS/8 );
 
 }
