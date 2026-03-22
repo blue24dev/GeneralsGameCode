796822761c77e8fffba8dac23effe1ff09aa1ec9
Blue Twentyfour <blue24dev@gmail.com>
2026-03-21 21:47:17 -0400

    update

--- GeneralsMD/Code/GameEngine/Source/Common/System/Upgrade.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/System/Upgrade.cpp
@@ -162,7 +162,7 @@ Int UpgradeTemplate::calcTimeToBuild( Player *player ) const
 #endif
 
 	//MODDD - for me only
-	return m_buildTime * 1.1 * LOGICFRAMES_PER_SECOND;
+	return m_buildTime * 1.1 * 1.4 * LOGICFRAMES_PER_SECOND;
 
 	///@todo modify this by power state of player
 	return m_buildTime * LOGICFRAMES_PER_SECOND;
