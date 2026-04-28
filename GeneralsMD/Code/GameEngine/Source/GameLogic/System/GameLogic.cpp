aad1efdbdecda3e2f7118e2518333fd7c2d6f39c
Blue Twentyfour <blue24dev@gmail.com>
2026-04-27 21:53:11 -0400

    Update GameLogic.cpp

--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogic.cpp
@@ -3062,37 +3062,8 @@ void GameLogic::processDestroyList()
 		Object* currentObject = (*iterator);
 
 		//MODDD - DEBUG - if anything is referring to this object being deleted... THAT'S BAD!
-		for (Object* objThru = TheGameLogic->getFirstObject(); objThru; objThru = objThru->getNextObject())
-		{
-			if (objThru->getContainedBy() != nullptr && objThru->getContainedBy() == currentObject)
-			{
-				std::ofstream outputFile;
-				outputFile.open("test_crash_containedByBadMemoryBug.txt", std::ios::out | std::ios::app);
-				printTimeStamp(outputFile);
-				outputFile << " - " << "processDestroyList" << std::endl;
-
-				printObjectIdentifyingInfo(outputFile, currentObject);
-				outputFile << " is being destroyed but ";
-				printObjectIdentifyingInfo(outputFile, objThru);
-				outputFile << "->getContainedBy() still refers to it" << std::endl;
-				
-				if (objThru->getID() == currentObject->getID())
-				{
-					outputFile << "WARNING - they match?" << std::endl;
-				}
-				
-				printDeletionCriticalInfo(outputFile, currentObject, "obj being deleted");
-				printDeletionCriticalInfo(outputFile, objThru, "obj w/ 'getContainedBy' pointing at that");
-				printItemsInContainedList(outputFile, currentObject, objThru);
-				outputFile.close();
-
-				// nope!  need to understand the issue as it happens right now, don't interfere with possible fixes yet
-				// (Trying the fix now!)
-				objThru->friend_setContainedBy(nullptr);
-				//printf("%d%d", isDestroyed, (int)conList);
-			}
-		}
-
+		objectContainedByOnDeleteCheck(currentObject, "processDestroyList");
+		
 #ifdef ALLOW_NONSLEEPY_UPDATES
 		for (std::list<UpdateModulePtr>::iterator it = m_normalUpdates.begin(); it != m_normalUpdates.end(); /* nothing */)
 		{
@@ -4759,36 +4730,7 @@ void GameLogic::_destroyObject( Object *obj )
 	}
 
 	//MODDD - DEBUG - if anything is referring to this object being deleted... THAT'S BAD!
-	for (Object* objThru = TheGameLogic->getFirstObject(); objThru; objThru = objThru->getNextObject())
-	{
-		Object* currentObject = obj;
-		if (objThru->getContainedBy() != nullptr && objThru->getContainedBy() == currentObject)
-		{
-			std::ofstream outputFile;
-			outputFile.open("test_crash_containedByBadMemoryBug.txt", std::ios::out | std::ios::app);
-			printTimeStamp(outputFile);
-			outputFile << " - " << "destroyObject" << std::endl;
-
-			printObjectIdentifyingInfo(outputFile, currentObject);
-			outputFile << " is being destroyed but ";
-			printObjectIdentifyingInfo(outputFile, objThru);
-			outputFile << "->getContainedBy() still refers to it" << std::endl;
-				
-			if (objThru->getID() == currentObject->getID())
-			{
-				outputFile << "WARNING - they match?" << std::endl;
-			}
-				
-			printDeletionCriticalInfo(outputFile, currentObject, "obj being deleted");
-			printDeletionCriticalInfo(outputFile, objThru, "obj w/ 'getContainedBy' pointing at that");
-			printItemsInContainedList(outputFile, currentObject, objThru);
-			outputFile.close();
-
-			// nope!  need to undersatnd the issue as it happens right now, don't interfere with possible fixes yet
-			//objThru->friend_setContainedBy(nullptr);
-			//printf("%d%d", isDestroyed, (int)conList);
-		}
-	}
+	objectContainedByOnDeleteCheck(obj, "destroyObject");
 
 
 }
